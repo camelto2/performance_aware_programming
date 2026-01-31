c80092b695b19f627d30ab4785da38a0e0cb1a73
Cody Melton <camelto2@gmail.com>
2026-01-30 18:21:23 -0700

    reads onebinary

--- decoder.c
+++ decoder.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int main(int argc, char* argv[])
 {
@@ -14,9 +15,29 @@ int main(int argc, char* argv[])
   FILE* fp = fopen(filename, "rb");
   if (fp == NULL)
   {
-    printf("ERROR: Could not open %s\n", fp);
+    printf("ERROR: Could not open file: %s. Exiting!\n", filename);
     return EXIT_FAILURE;
   }
+
+  //2 8 bit vals
+  uint8_t val[2];
+  size_t bytes_read;
+
+  while ( (bytes_read = fread(&val, sizeof(val[0]), sizeof(val)/sizeof(val[0]), fp)) == sizeof(val)) 
+  {
+    printf("Read %x %x\n", val[0], val[1]);
+  }
+
+  if (ferror(fp))
+  {
+    perror("Error reading file");
+  }
+  else if (bytes_read > 0 && bytes_read < sizeof(val[0]))
+  {
+    printf("Warning: end of file with incomplete 2byte chunk (%zu bytes read)\n", bytes_read);
+  }
+
+  fclose(fp);
 
   return EXIT_SUCCESS;
 }
